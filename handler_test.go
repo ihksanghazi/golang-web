ccb66a6b6dc37a00b5fb0e2ad82c3bad95175d4f
Nursandy Ihksan <nursandyihksan97@gmail.com>
2023-06-27 17:33:01 +0700

    serveMux

--- handler_test.go
+++ handler_test.go
@@ -24,3 +24,33 @@ func TestHandler(t *testing.T){
 	}
 
 }
+
+func TestServeMux(t *testing.T) {
+	mux:= http.NewServeMux()
+
+	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request){
+		fmt.Fprint(writer, "Hellow World")
+	})
+
+	mux.HandleFunc("/hi", func(writer http.ResponseWriter, request *http.Request){
+		fmt.Fprint(writer, "Hi")
+	})
+
+	mux.HandleFunc("/images/", func(writer http.ResponseWriter, request *http.Request){
+		fmt.Fprint(writer, "Images")
+	})
+
+	mux.HandleFunc("/images/thumbnail", func(writer http.ResponseWriter, request *http.Request){
+		fmt.Fprint(writer, "Thumbnail")
+	})
+
+	server:= http.Server{
+		Addr: "localhost:8080",
+		Handler: mux,
+	}
+
+	err:= server.ListenAndServe()
+	if err!= nil{
+		panic(err)
+	}
+}
