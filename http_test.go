dfc530446962f538f26e6e48b26d36c19e21279a
Nursandy Ihksan <nursandyihksan97@gmail.com>
2023-06-27 20:40:43 +0700

    httpTest

---
+++ http_test.go
@@ -0,0 +1,27 @@
+package golang_web
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func helloHandler(writer http.ResponseWriter, request *http.Request){
+	fmt.Fprint(writer, "hello World")
+}
+
+func TestHttp (t *testing.T){
+	request := httptest.NewRequest(http.MethodGet,"http://localhost:8080/hello",nil)
+	recorder := httptest.NewRecorder()
+
+	helloHandler(recorder,request)
+
+	response:= recorder.Result()
+	body,_:=io.ReadAll(response.Body)
+
+	bodyString := string(body)
+
+	fmt.Println(bodyString)
+}
