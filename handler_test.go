44d69995dd52ff107a2094e425a3818a2cc56100
Nursandy Ihksan <nursandyihksan97@gmail.com>
2023-06-27 20:26:16 +0700

    request

--- handler_test.go
+++ handler_test.go
@@ -54,3 +54,20 @@ func TestServeMux(t *testing.T) {
 		panic(err)
 	}
 }
+
+func TestRequest(t *testing.T) {
+	var handler http.HandlerFunc = func(writer http.ResponseWriter, request *http.Request){
+		fmt.Fprint(writer, request.Method)
+		fmt.Fprint(writer, request.URL)
+	}
+
+	server:= http.Server{
+		Addr: "localhost:8080",
+		Handler: handler,
+	}
+
+	err:=server.ListenAndServe()
+	if err!= nil{
+		panic(err)
+	}
+}
