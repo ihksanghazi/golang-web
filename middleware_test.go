f06c5232687e610cac0a548e3f0a05f06dccd8aa
Nursandy Ihksan <nursandyihksan97@gmail.com>
2023-07-03 12:27:09 +0700

    middleware

---
+++ middleware_test.go
@@ -0,0 +1,68 @@
+package golang_web
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+type LogMiddleware struct {
+	Handler http.Handler
+}
+
+func (middleware *LogMiddleware) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	fmt.Println("Before Execute Handler")
+	middleware.Handler.ServeHTTP(writer, request)
+	fmt.Println("After Execute Handler")
+}
+
+type ErrorHandler struct{
+	Handler http.Handler
+}
+
+func (errorHandler *ErrorHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request){
+	defer func(){
+		err:= recover()
+		if err != nil{
+			fmt.Println("Terjadi Error")
+			writer.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(writer,"Error : %s",err)
+		}
+	}()
+	errorHandler.Handler.ServeHTTP(writer,request)
+}
+
+func TestMiddleware(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Println("Handler Executed")
+		fmt.Fprintln(writer, "Hello Middleware")
+	})
+	mux.HandleFunc("/jigong", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Println("Jigong Executed")
+		fmt.Fprintln(writer, "Hello Jigong")
+	})
+	mux.HandleFunc("/panic", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Println("Jigong Executed")
+		panic("Hayooooo")
+	})
+
+	logMiddleware := &LogMiddleware{
+		Handler: mux,
+	}
+
+	errorHandler := &ErrorHandler{
+		Handler: logMiddleware,
+	}
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: errorHandler,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+
